0ffa9a7c40520ca10448836242eba4de3adb38cb
River <river0617@slogup.com>
2022-07-21 11:42:58 +0900

    chore: signUp 페이지로 이동시 약관데이터 초기화

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Back } from "../assets";
+import urls from "../constants/urls";
+import { useAppDispatch } from "../store/hook";
+import { addCluAgrList } from "../store/modules/User";
 
 interface HeadTitle {
   title?: string;
@@ -9,12 +12,17 @@ interface HeadTitle {
 
 function Header({ title, backBtn }: HeadTitle) {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   return (
     <header className="sticky top-0 z-10 flex items-center justify-center w-full bg-white border-b max-w-480 min-w-360 h-61 border-grey-200">
       {backBtn && (
         <button
           className="absolute left-20"
           onClick={() => {
+            if (window.location.pathname === urls.AccepTerms) {
+              dispatch(addCluAgrList([]));
+            }
             navigate(-1);
           }}
           type="button"
