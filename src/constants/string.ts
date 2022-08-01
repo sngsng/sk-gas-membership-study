6a0c14db93cce4ca654f5fe5fd16fc865c77f16b
River <river0617@slogup.com>
2022-08-01 14:58:41 +0900

    chore: 자주 쓰이는 텍스트 처리

---
+++ src/constants/string.ts
@@ -0,0 +1,69 @@
+export default {
+  // header
+  MobileMembershipRegistration: "행복충전모바일 회원가입",
+
+  // signUp
+  // title
+  AcceptTheFullTerms: "전체 약관에 동의합니다.",
+  Authentication: "본인인증",
+  FullyAgreeTerms: "본인인증 약관에 전체 동의합니다.",
+
+  Id: "아이디 *",
+  EnterID: "아이디를 입력해주세요",
+
+  Pwd: "비밀번호 *",
+  EnterPwd: "비밀번호를 입력해주세요",
+  RePwd: "비밀번호 재입력 *",
+
+  CarNumber: "차량번호 *",
+  EnterCarNumber: "차량번호를 입력해주세요(ex 00가 0000)",
+
+  Name: "이름 *",
+  EnterName: "이름을 입력해주세요",
+
+  Birth: "생년월일 *",
+  EnterBirth: "생년월일을 입력해주세요",
+
+  Agency: "통신사 *",
+  EnterAgency: "통신사를 선택해주세요",
+
+  PhoneNumber: "휴대폰 *",
+  EnterPhoneNumber: "휴대폰 번호를 입력해주새요",
+
+  Gen: "성별 *",
+  Man: "남자",
+  Woman: "여자",
+
+  // error 메세지
+  IdErrorMessage: "영문 대소문자, 숫자를 조합하여 5글자 이상 입력해주세요.",
+  PassWordErrorMessage:
+    "영문 대소문자, 숫자, 특수문자를 포함하여 8이상 입력해주세요",
+  PassWordsNotMatch: "비밀번호가 일치 하지 않습니다",
+
+  CarErrorMessage: "형식이 올바르지 않습니다 (ex 00가0000)",
+
+  EnterMoreThan2: "2글자 이상 입력해주세요",
+
+  Enter8BirthDate: "생년월일 8자리를 입력해주세요",
+
+  Enter11PhoneMessage: "휴대폰 11자리를 입력해주세요",
+
+  // 중복 확인 메세지
+  AvailableID: "사용가능한 아이디입니다.",
+
+  DuplicateID: "중복된 아이디 입니다.",
+
+  // button 메세지
+  Next: "다음",
+  Check: "확인",
+
+  AgreeAndSignUp: "동의하고 회원가입",
+  CheckForDuplication: "중복확인",
+
+  // 인증관련
+  AuthenticateWithPhone: "휴대폰으로 인증하기",
+  AuthenticateWithApp: "PASS 앱으로 인증완료",
+
+  // 마케팅 메세지
+  //  :"※ 본 마케팅 수신 동의 시 혜택으로 충전 할인 서비스가 제공됩니다."
+};
